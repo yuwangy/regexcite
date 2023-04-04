789d621bef3f7df364a331621016926c64d322a3
Yuyang Wang <franktcsn@gmail.com>
2023-04-03 19:51:53 -0700

    add tests for strsplit function

--- tests/testthat/test-strsplit1.R
+++ tests/testthat/test-strsplit1.R
@@ -1,3 +1,11 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
+
+test_that("strsplit1() splits a string", {
+  expect_equal(strsplit1("a,b,c", split = ","), c("a", "b", "c"))
+})
+
+test_that("strsplit1() splits a string", {
+  expect_equal(strsplit1("tom,jerry", split = ","), c("tom", "jerry"))
+})
