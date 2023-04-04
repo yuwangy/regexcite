82367f771e5a91f3cd541f17b84f5bea0be358e8
Yuyang Wang <franktcsn@gmail.com>
2023-04-03 21:26:40 -0700

    modify test file for my_add

--- tests/testthat/test-my_add.R
+++ tests/testthat/test-my_add.R
@@ -5,3 +5,28 @@ test_that("multiplication works", {
 test_that("my_add() sums two values", {
   expect_equal(my_add(2,3), 5)
 })
+
+test_that("my_add() sums two values", {
+  expect_equal(my_add(-45,3453), 3408)
+})
+
+test_that("my_add() sums two values", {
+  expect_equal(my_add(12), 22)
+})
+
+test_that("my_add() sums two values", {
+  expect_equal(my_add(NA), NA)
+})
+
+test_that("my_add() sums two values", {
+  expect_equal(my_add(12, NA), NA)
+})
+
+test_that("my_add() sums two values", {
+  expect_error(my_add("68",3))
+})
+
+test_that("my_add() sums two values", {
+  expect_error(my_add("dsfe","55"))
+})
+
