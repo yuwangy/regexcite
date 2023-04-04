34f590e4a59fc88d31ccc90515da2e9c84611c57
Yuyang Wang <franktcsn@gmail.com>
2023-04-03 19:42:20 -0700

    add document() for function strsplit1

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,5 +1,18 @@
 # strsplit1() function allows you to specify the splitting delimiter, such as 1.
 
+#' Split , divide a string
+#'
+#' @param x : A character vector
+#' @param split : split the string
+#'
+#' @return : A character vector
+#' @export
+#'
+#' @examples
+#' x <- "alfa, brave, charlie"
+#' strsplit1(x, split = ",")
+#'
+
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
