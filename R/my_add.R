526a0ee3692e54225275c82aadb0a079441faca9
Yuyang Wang <franktcsn@gmail.com>
2023-04-03 20:59:07 -0700

    modify my_add function

--- R/my_add.R
+++ R/my_add.R
@@ -2,10 +2,10 @@
 #y <- 10
 
 my_add <- function(x,y = 10){
-  if (is.numeric(x) && is.numeric(y)){
-    sum(x, y)
-  } else {
+  if (!is.numeric(x) || !is.numeric(y)){
     stop("One of your inputs contains a string value")
+  } else {
+    sum(c(x,y), na.rm = TRUE)
   }
 }
 
@@ -15,4 +15,6 @@ my_add(9)
 my_add(10, NA)
 my_add(NA)
 my_add("43","242")
+my_add("56", 33)
+my_add("sdf",59584)
 
